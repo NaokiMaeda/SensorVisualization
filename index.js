acf0629937588d235e070072ab737c9437c2dad6
Maeda Naoki <maeda.naoki.md9@is.naist.jp>
2016-05-08 16:03:18 +0900

    Connect InfluxDB
    connect localhost InfluxDB

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var influx = require("influx");
+var fs = require("fs");
+
+var serverInfo = JSON.parse(fs.readFileSync("serverInfo.json" , "utf-8"));
+var client = influx(serverInfo);
 
 //require('crash-reporter').start();
 
@@ -11,19 +16,16 @@ app.on('window-all-closed', function() {
   app.quit();
 });
 
-var data;
-
 app.on('ready', function() {
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   setInterval(function(){
-    data = Math.random()*100;
-    mainWindow.webContents.send("influx" , data);
-    console.log(data);
+    client.query("select HeartRate , Temparture from testData",function(err , result){
+      console.log(result);
+    });
   },1000);
-  
 
   mainWindow.on('closed', function() {
   mainWindow = null;
