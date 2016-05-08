48c74bf3a853485abec57ebbc4e059a0118386e4
Maeda Naoki <maeda.naoki.md9@is.naist.jp>
2016-05-08 13:42:38 +0900

    Update Chart Data
    chart redraw when chart data changed

--- JS/chart.js
+++ JS/chart.js
@@ -1,23 +1,41 @@
 var data = {
     labels: ["1月","2月","3月","4月","5月","6月","7月"],//X軸のラベル
-     datasets: [{
-        label: "My First dataset",//項目名
-        fillColor: "rgba(220,220,220,0.5)",//塗りつぶす色
-        strokeColor: "rgba(220,220,220,0.8)",//枠線の色
-        highlightFill: "rgba(220,220,220,0.75)",//マウスオーバー時塗りつぶす色
-        highlightStroke: "rgba(220,220,220,1)",//マウスオーバー時枠線の色
-    data: [65, 59, 80, 81, 56, 55, 40]//値
+    datasets: [{
+        label: "My First dataset",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]//値
     }]
 };
 
 var ctx = document.getElementById("canvas").getContext("2d");
 
-new Chart(ctx, {
+var lineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: {
         xAxes: [{
-            display: false
+            display: true
         }]
     }
 });
+
+setInterval(function(){
+    lineChart.data.datasets[0].data[0] = Math.random() * 100;
+    lineChart.update();
+} , 1000);
--- index.js
+++ index.js
@@ -12,12 +12,14 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+  mainWindow.webContents.send("influx" , "data");
+  console.log("sended");
+
   mainWindow.on('closed', function() {
-    mainWindow = null;
+  mainWindow = null;
   });
 });
